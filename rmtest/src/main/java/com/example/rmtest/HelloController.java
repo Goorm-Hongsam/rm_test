35dee3a2f1376749634fe244efb4eaddaa4bfb24
dbstn1325 <dbstn1325@naver.com>
2023-11-04 18:22:18 +0900

    feat: user

---
+++ rmtest/src/main/java/com/example/rmtest/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.rmtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping("/")
+
+    public Long getUser() {
+        return 1L;
+    }
+}
