6fe5276bc45fc0d4dfbf5b1a23977d8684c11da4
dbstn1325 <dbstn1325@naver.com>
2023-11-04 17:40:35 +0900

    first Commit

---
+++ rmtest/src/test/java/com/example/rmtest/RmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
